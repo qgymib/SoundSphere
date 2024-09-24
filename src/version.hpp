f3f1a02e8edc752b87d432dfd7bd1938c0a3ddc3
qgymib <4520263+qgymib@users.noreply.github.com>
2024-09-24 14:44:12 +0800

    Release v1.0.0

--- src/version.hpp
+++ src/version.hpp
@@ -1,12 +1,16 @@
 #ifndef SOUND_SPHERE_VERSION_HPP
 #define SOUND_SPHERE_VERSION_HPP
 
-#define SOUND_SPHERE_VERSION_MAJOR  0
+#define SOUND_SPHERE_VERSION_MAJOR  1
 #define SOUND_SPHERE_VERSION_MINOR  0
-#define SOUND_SPHERE_VERSION_PATCH  1
+#define SOUND_SPHERE_VERSION_PATCH  0
 
 namespace soundsphere {
 
+/**
+ * @brief Version string.
+ * @return C string.
+ */
 const char* version(void);
 
 }
