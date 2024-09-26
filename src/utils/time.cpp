1aa00bc23eb4d12434edfa1417784ad1c385eb3a
qgymib <4520263+qgymib@users.noreply.github.com>
2024-09-26 09:29:47 +0800

    Fix build error on unix

--- src/utils/time.cpp
+++ src/utils/time.cpp
@@ -1,18 +1,20 @@
 #include <ev.h>
 #include <cstdio>
+#include <cassert>
 #include "time.hpp"
 
 void soundsphere::time_seconds_to_string(char* buff, size_t size, double seconds)
 {
-    int hours = static_cast<int>(seconds) / 3600;
-    int minutes = (static_cast<int>(seconds) % 3600) / 60;
-    int secs = static_cast<int>(seconds) % 60;
-    snprintf(buff, size, "%02d:%02d:%02d", hours, minutes, secs);
+    assert(seconds >= 0);
+    unsigned hours = static_cast<unsigned>(seconds) / 3600;
+    unsigned minutes = (static_cast<unsigned>(seconds) % 3600) / 60;
+    unsigned secs = static_cast<unsigned>(seconds) % 60;
+    snprintf(buff, size, "%02u:%02u:%02u", hours, minutes, secs);
 }
 
 std::string soundsphere::time_seconds_to_string(double seconds)
 {
-    char buf[12];
+    char buf[32];
     time_seconds_to_string(buf, sizeof(buf), seconds);
     return buf;
 }
